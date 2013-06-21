c5fe37ebb56faee079bc806021df4bd1c015f0cc
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-21 16:54:00 -0400

    fixed restaurant menu upload

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :street, :city, :state, :zip, :description, :name, :phone, :image, :remote_image_url, :menu
   mount_uploader :image, ImageUploader
-  mount_uploader :menu, ImageUploader
+  mount_uploader :menu, MenuUploader
   
   def address
     "#{self.street} #{self.city}, #{self.state} #{self.zip}"
