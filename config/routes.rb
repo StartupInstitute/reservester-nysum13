8e9cb94016f645d3cefbc4c1f260bcdc659b8f31
Huge Ma <hugobomber@gmail.com>
2013-06-21 16:40:26 -0500

    generated views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Reservester::Application.routes.draw do
+  resources :restaurants
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Reservester::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'restaurants#index'
 
   # See how all your routes lay out with "rake routes"
 
