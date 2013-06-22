1476d0eaffe4c471ffcc911393bef398d5484af5
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-22 17:24:14 -0400

    installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Reservester::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  # config.action_mailer.default_url_options = { :host => 'TBD' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
