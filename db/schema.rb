42fec9e570d96f2beb50aab6031e5c9b34bb4b21
Budi Sugianto <budisugianto777@gmail.com>
2013-06-22 12:49:54 -0400

    Change the view of restaurant and add migration image and menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622013336) do
+ActiveRecord::Schema.define(:version => 20130622155244) do
 
   create_table "restaurants", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20130622013336) do
     t.string   "phone"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image"
+    t.string   "menu"
   end
 
 end
