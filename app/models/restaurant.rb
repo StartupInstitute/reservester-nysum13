46056365a3aaa0415e3a976d4dfd4ad8753bd733
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-22 15:52:34 -0400

    Centered google map

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,7 +10,7 @@ class Restaurant < ActiveRecord::Base
   def google_map
     street = self.street.gsub(" ", "+") if !self.street.nil?
     city = self.city.gsub(" ", "+") if !self.city.nil?
-    "http://maps.googleapis.com/maps/api/staticmap?center=#{street},#{city},#{state}&zoom=13&size=450x300&maptype=roadmap
+    "http://maps.googleapis.com/maps/api/staticmap?center=#{street},#{city},#{state}&zoom=13&size=550x400&maptype=roadmap
     &markers=label%7size:small%7Ccolor:blue%7C#{street},#{city},#{state}\&sensor=false"
   end
   
