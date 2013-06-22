5280e74617666e2c8e7087af6e1281e951d703ff
Elvin G. Marmol <egm0921@gmail.com>
2013-06-22 17:30:46 -0400

    Fixed views to include necessary links. Created form view.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,12 @@ class RestaurantsController < ApplicationController
 	end
 
 	def show
-		@restaurants = Restaurant.find(params[:id])
+		@restaurant = Restaurant.find(params[:id])
+
+		respond_to do |format|
+      		format.html # show.html.erb
+      		format.json { render json: @restaurant }
+    	end
 	end
 	
 	def new
@@ -22,23 +27,39 @@ class RestaurantsController < ApplicationController
 		respond_to do |format|
 			if @restaurant.save
 				format.html { redirect_to @restaurant, notice: 'Restaurant added!' }
-				format.json { render json: @fact, status: :created, location: @fact }
+				format.json { render json: @restaurant, status: :created, location: @restaurant }
       		else
         		format.html { render action: "new" }
-        		format.json { render json: @fact.errors, status: :unprocessable_entity }
+        		format.json { render json: @restaurant.errors, status: :unprocessable_entity }
       		end
       	end
 	end
 	
 	def edit
-
+		@restaurant = Restaurant.find(params[:id])
 	end
 	
 	def update
+		@restaurant = Restaurant.find(params[:id])
 
+		respond_to do |format|
+			if @restaurant.update_attributes(params[:restaurant])
+				format.html { redirect_to @restaurant, notice: "Restaurant Update Complete."}
+				format.json { head :no_content}
+			else
+				format.html { render action: "edit" }
+				format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 	
 	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
 
+		respond_to do |format|
+			format.html { redirect_to restaurants_path }
+			format.json { head :no_content }
+		end 
 	end
 end
