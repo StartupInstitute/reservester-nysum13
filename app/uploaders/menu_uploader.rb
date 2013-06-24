8a7ad054080c2c7e20c55e9112ec951d12d3bc18
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-24 16:42:56 -0400

    made filefile upload storage type consistent

--- app/uploaders/menu_uploader.rb
+++ app/uploaders/menu_uploader.rb
@@ -11,9 +11,12 @@ class MenuUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
-  # storage :file
-
+  if Rails.env == "production"
+    storage :fog
+  elsif Rails.env == "development"
+    storage :file
+  end
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
