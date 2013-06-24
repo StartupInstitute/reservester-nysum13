676217fe86f05bc5d4e4fa355f113998681e9a36
danie <myubuntu@myubuntu.(none)>
2013-06-24 14:46:08 -0400

    Starting Lab 2 over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reservester::Application.config.secret_token = '566c32695bbc915ad8579bc3320e7723c671cc6bfdbe1f126722e96f83b2fe9e95e71e47c97e87edb6b1061a0944a793af43b2da1757e1fe8b1dc2e034129faa'
