a44ec4fa7ee9bd7b5710c87d4224b2358f1f4687
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-25 15:01:59 -0400

    added owner dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Reservester::Application.routes.draw do
   devise_for :owners do
     get "/owners/sign_out", to: "devise/sessions#destroy"
   end
-
-  get "pages/index"
+  
+  get "dashboard/show"
   
   resources :restaurants do
     resources :reservations
   end
+  
+  get "pages/index"
   
   root to: "pages#index"
  
