d432290ae7d7109884ca99cd83b71374e983a2d8
Bob Breznak <bob.breznak@gmail.com>
2013-06-25 22:48:25 -0400

    Add validations for Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,6 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :email, :message, :reserve_on, :restaurant_id
+
+  validates :reserve_on, :presence => true
+  validates :email, :presence => true, :email => true
 end
