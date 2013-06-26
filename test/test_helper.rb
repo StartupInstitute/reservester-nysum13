1f3997ba853067609866e1d8b0017925c8d278b0
Bob Breznak <bob.breznak@gmail.com>
2013-06-25 23:25:19 -0400

    Add tests for Reservation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+end
+
+class ActionController::TestCase
   include Devise::TestHelpers
 end
