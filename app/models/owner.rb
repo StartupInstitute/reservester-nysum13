0856f0cc0aa2ad31d88348e5792420df5ffd67b9
Huge Ma <hugobomber@gmail.com>
2013-06-26 01:36:52 -0400

    created owner:restaurant relationship

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,6 +5,8 @@ class Owner < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :restaurants
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :description, :name, :phone, :photo, :photo_cache
 
+  belongs_to :owner
+
   validates :name, :presence => true
   validates :phone, :presence => true
 
