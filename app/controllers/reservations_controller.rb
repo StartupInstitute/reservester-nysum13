3fc460fe0ca2dad58dc8147426a59c6fc343b936
Budi Sugianto <budisugianto777@gmail.com>
2013-06-28 15:50:37 -0400

    Add the security layer with figaro gem

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,7 +17,7 @@ class ReservationsController < ApplicationController
   def create
     restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = restaurant.reservations.create(params[:reservation])
-    if @reservation.save
+    if verify_recaptcha() and @reservation.save
       redirect_to [@reservation.restaurant, @reservation] , :notice => 'Reservation was successfully created. '
       ReservationMailer.reservation_confirmation(@reservation).deliver
     else
