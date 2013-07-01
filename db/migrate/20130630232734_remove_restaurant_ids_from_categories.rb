5cb3fa09b1e1bf64691f0bb94ca725ffb70a9441
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-01 16:38:39 -0400

    deleted migration

--- db/migrate/20130630232734_remove_restaurant_ids_from_categories.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRestaurantIdsFromCategories < ActiveRecord::Migration
-  def up
-    remove_column :categories, :restaurant_id
-  end
-
-  def down
-    add_column :categories, :restaurant_id
-  end
-end
