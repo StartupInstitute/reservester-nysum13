408c5244e90941a20e4bde38b0b48adb48452bbf
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-06-30 21:51:22 -0400

    commit  before heroku

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,12 @@
 class Restaurant < ActiveRecord::Base
+	
   attr_accessible :address, :description, :name, :phone, :image, :menu
-  
+
+  validates_presence_of :name, :address, :description, :phone
+	validates_uniqueness_of :address, :case_sensitive => false
+	validates_uniqueness_of :phone
+	validates_format_of :phone, :with => /\A[0-9]{10}\Z/
+	
   mount_uploader :image, ImageUploader
 
   mount_uploader :menu, MenuUploader
