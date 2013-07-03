5cc725064db924deaf7d1a2817a05e4abc588bcb
Elvin G. Marmol <egm0921@gmail.com>
2013-07-03 17:27:13 -0400

    Changed seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-owner1 = Owner.new(:name => 'Test Owner1', :email => 'egm0921@gmail.com', :password => 'test123')
+owner1 = Owner.create(:name => 'Test Owner1', :email => 'egm0921@gmail.com', :password => 'test123')
 
 owner1_restaurant_list = [
 	["Black Iron Burger", "212-677-6067", "540 E 5th St New York, NY 10009", "Great burger and bar in the East Village. Iron Horse is their signature burger."],
@@ -17,7 +17,7 @@ owner1_restaurant_list.each do |name, number, address, description|
 	owner1.restaurants.create(name: name, number: number, address: address, description: description)
 end
 
-owner2 = Owner.new(:name => 'Test Owner2', :email => 'emurphh@gmail.com', :password => 'test123')
+owner2 = Owner.create(:name => 'Test Owner2', :email => 'emurphh@gmail.com', :password => 'test123')
 
 owner2_restaurant_list = [
 	["Luz Restaurant", "718-246-4000", "177 Vanderbilt Ave Brooklyn NY 11205", "Nuevo Latino dishes sporting “interesting flavor combos” team up with “great” cocktails to deliver “reasonably priced” good times at this Fort Greene find; granted, it’s “slightly out of the way”, but fans say it’s “worth the trip” for a “zesty” meal in “cool”, “unpretentious” environs."],
