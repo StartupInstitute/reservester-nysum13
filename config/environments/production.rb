d174611f18427d85852862db816742632b602d73
Huge Ma <hugobomber@gmail.com>
2013-07-09 13:09:13 -0400

    change static assets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Reservester::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   # config.assets.initialize_on_precompile = false #precompile fix 1
   # Compress JavaScripts and CSS
   config.assets.compress = true
