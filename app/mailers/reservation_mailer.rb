28a614dd38b4bdfc6dcd8cc5123e21f2a9b6e19a
Budi Sugianto <budisugianto777@gmail.com>
2013-07-10 16:37:57 -0400

    Add SMTP from for

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,6 +2,8 @@ class ReservationMailer < ActionMailer::Base
 
   def reservation_confirmation(reservation)
      @reservation = reservation
-    mail(to: reservation.reserver_email , subject: "Your Reservation Confirmed")
+    mail(to: reservation.reserver_email ,
+         from: "budisugianto777@gmail.com",
+         subject: "Your Reservation Confirmed")
   end
 end
