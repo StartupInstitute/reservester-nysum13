f51113d5c330aeed31259086238f0a64badc0075
daniehacks <daniebanks718@gmail.com>
2013-07-20 18:28:41 -0400

    updated production host

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
 	address: 'smtp.gmail.com', 
 	port: 587, 
-	domain: 'reservester.com', 
+	domain: 'immense-lake-9635.herokuapp.com', 
 	user_name: ENV["EMAIL"], 
 	password: ENV["PASSWORD"], 
 	authentication: 'plain', 
